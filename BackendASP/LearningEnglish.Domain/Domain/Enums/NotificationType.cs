cd495abb3c524c6f4831796a7f60ccbadd8fcaa5
Minhxoan24 <142246049+Minhxoan24@users.noreply.github.com>
2025-12-11 01:33:27 +0700

    linh tinh

---
+++ BackendASP/LearningEnglish.Domain/Domain/Enums/NotificationType.cs
@@ -0,0 +1,16 @@
+namespace LearningEnglish.Domain.Enums
+{
+    public enum NotificationType
+    {
+        CourseEnrollment,
+        CourseCompletion,
+        LessonReminder,
+        QuizDeadline,
+        AssessmentGraded,
+        PaymentSuccess,
+        SystemAnnouncement,
+        StudyStreak,
+        Achievement,
+        General
+    }
+}
