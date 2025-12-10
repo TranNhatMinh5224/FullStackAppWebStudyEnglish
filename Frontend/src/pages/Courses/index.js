5c27dd706f6a0a3654842b72259468cf0686e3c1
Minhxoan24 <142246049+Minhxoan24@users.noreply.github.com>
2025-12-10 23:31:16 +0700

    delete Bin

--- Frontend/src/pages/Courses/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CoursesPage } from './CoursesPage';
-export { default as CourseDetailPage } from './CourseDetailPage';
-export { default as MyCoursesPage } from './MyCoursesPage';
